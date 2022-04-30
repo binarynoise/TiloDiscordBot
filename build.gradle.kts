2d509eda72ef5ddf4eb4edd509ec4d84edb432fe
binarynoise <binarynoise@mail.de>
2022-04-30 15:18:50 +0200

    add central configuration for JVM version and jar properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,24 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.20"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    idea
+    kotlin("jvm") version "1.6.21"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+}
+
+val javaVersion = JavaVersion.VERSION_1_8
+val javaVersionNumber = javaVersion.name.substringAfter('_').replace('_', '.')
+val javaVersionMajor = javaVersion.name.substringAfterLast('_')
+
+idea {
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+        languageLevel = IdeaLanguageLevel(javaVersion)
+        targetBytecodeVersion = javaVersion
+    }
 }
 
 repositories {
@@ -15,10 +30,7 @@ dependencies {
     // Align versions of all Kotlin components
     implementation(platform(kotlin("bom")))
     
-    // Use the Kotlin JDK 8 standard library.
-    implementation(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
     
     implementation("dev.kord:kord-core:0.8.0-M12")
     implementation("org.slf4j:slf4j-simple:1.7.36")
@@ -27,19 +39,26 @@ dependencies {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = javaVersionNumber
+}
+
+tasks.withType<AbstractCompile> {
+    sourceCompatibility = javaVersionNumber
+    targetCompatibility = javaVersionNumber
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes(mapOf("Main-Class" to "Main"))
+    }
 }
 
 tasks.withType<ShadowJar> {
-    archiveClassifier.set("")
-    archiveVersion.set("standalone")
+    archiveClassifier.set("shadow")
     mergeServiceFiles()
     minimize {
         exclude(dependency("dev.kord:kord-core:.*"))
         exclude(dependency("org.slf4j:slf4j-simple:.*"))
-    }
-    manifest {
-        attributes(mapOf("Main-Class" to "MainKt"))
     }
 }
 
